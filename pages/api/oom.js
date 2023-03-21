9efd5dffb97c929a6af79cdfead49fad62988ba8
Fernando Dingler <fdingler@amazon.com>
2023-03-21 15:35:00 -0700

    oom

---
+++ pages/api/oom.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+    // FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory
+    let memory = [];
+    for (let i = 0; i < 100000000; i++) {
+        if (i % 1000000 === 0) {
+            console.log("Memory size: " + memory.length);
+        }
+
+        memory.push(`aaaaaaaaaaaaaaaaaa${i}`);
+    }
+
+    res.status(200).json({ memory: memory.length });
+}
+  
