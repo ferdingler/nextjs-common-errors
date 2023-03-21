be2118739e20325e00c09c6e80cf7b48dd68df67
Fernando Dingler <fdingler@amazon.com>
2023-03-21 16:59:30 -0700

    oom2

--- pages/api/oom.js
+++ pages/api/oom.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
             console.log("Memory size: " + memory.length);
         }
 
-        memory.push(`aaaaaaaaaaaaaaaaaa${i}`);
+        memory.push(`aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa${i}`);
     }
 
     res.status(200).json({ memory: memory.length });
